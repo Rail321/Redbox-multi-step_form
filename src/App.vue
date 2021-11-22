43fef5098c2a98bc04a4bc60828b1d5273c2d5e7
Rail Shakurov <shakurov-98@mail.ru>
2021-11-23 01:19:51 +0300

    init; some UI-elements creating; step logic; image uploading logic; submitting alert

---
+++ src/App.vue
@@ -0,0 +1,281 @@
+<template>
+  <main class="main h-100">
+    <form
+      v-on:submit.prevent="submit"
+      v-on:reset.prevent="reset"
+    >
+      <StepOne
+        v-if="!isFormCompleted"
+        v-bind:isSubmissionSuccessful="isSubmissionSuccessful"
+        v-on:next="next"
+        v-on:setImage="setImage"
+      />
+
+      <StepTwo
+        v-else
+        v-bind:imageUrl="imageUrl"
+        v-on:prev="prev"
+      />
+    </form>
+  </main>
+</template>
+
+<script>
+  import StepOne from './views/StepOne.vue'
+  import StepTwo from './views/StepTwo.vue'
+
+  export default {
+    components: { StepOne, StepTwo },
+
+    data() {
+      return {
+        isFormCompleted: false,
+        isSubmissionSuccessful: false,
+
+        imageUrl: null,
+      }
+    },
+
+    methods: {
+      next() {
+        this.isFormCompleted = true
+        this.isSubmissionSuccessful = false
+      },
+
+      reset() {
+        this.isSubmissionSuccessful = false
+      },
+
+      prev() {
+        this.isFormCompleted = false
+      },
+
+      submit() {
+        this.isSubmissionSuccessful = true
+        this.isFormCompleted = false
+      },
+
+      setImage(event) {
+        const [ image ] = event.target.files
+        
+        if(image) {
+          this.imageUrl = URL.createObjectURL(image)
+        }
+      },
+    },
+  }
+</script>
+
+<style lang="scss">
+  // Reset
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      vertical-align: baseline;
+  }
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+      display: block;
+  }
+  html {
+      height: 100%;
+  }
+  body {
+      line-height: 1;
+  }
+  ol, ul {
+      list-style: none;
+  }
+  blockquote, q {
+      quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+      content: '';
+      content: none;
+  }
+  table {
+      border-collapse: collapse;
+      border-spacing: 0;
+  }
+
+  // Variables
+  $color-1: #F8F8FA;
+  $color-2: #FFF;
+  $color-3: #9B63F8;
+  $color-4: #211536;
+  $color-5: #CDB1FB;
+  $hover-change: 10;
+  $active-change: 20;
+  $transition: 0.2s all;
+  $col-padding: 10px;
+  $border-radius: 4px;
+
+  // Helpers
+  .h-100 {
+    height: 100%;
+  }
+  .min-vh-100 {
+    min-height: 100vh;
+  }
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  .d-flex {
+    display: flex;
+    margin: -$col-padding;
+  }
+  .flex-column {
+    flex-direction: column;
+  }
+  .align-center {
+    align-items: center;
+  }
+  .flex-child {
+    padding: $col-padding;
+  }
+  .flex-child-4 {
+    flex-basis: 33.33%;
+  }
+  .flex-grow-1 {
+    flex-grow: 1;
+  }
+  .w-100 {
+    width: 100%;
+  }
+
+  // UI
+  .btn {
+    display: inline-block;
+    width: 285px;
+    max-width: 100%;
+    padding: 18px 19px;
+    border-radius: $border-radius;
+    border: 1px solid $color-3;
+    color: $color-4;
+    background-color: $color-2;
+    cursor: pointer;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 20px;
+
+    &:hover {
+      transition: $transition;
+      border-color: darken($color-3, $hover-change);
+      background-color: darken($color-2, $hover-change);
+    }
+
+    &_filled {
+      background-color: $color-3;
+      color: $color-2;
+
+      &:hover {
+        border-color: darken($color-3, $hover-change);
+        background-color: darken($color-3, $hover-change);
+      }
+    }
+  }
+  .h2 {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 22px;
+    color: $color-4;
+  }
+  .input-text {
+    padding: 18px 25px;
+    border: 1px solid $color-5;
+    border-radius: $border-radius;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 20px;
+    color: $color-4;
+  }
+
+  // Main
+  .main {
+    background-color: $color-1;
+  }
+
+  // Section
+  .section {
+    &-inner {
+      padding: 40px 30px 50px;
+      background-color: $color-2;
+    }
+    &__alert{
+      margin-bottom: 60px;
+    }
+    &__h2 {
+      margin-bottom: 30px;
+    }
+  }
+
+  // Alert
+  .alert {
+    padding: 20px 25px;
+    background-color: $color-2;
+    box-shadow: 0px 8px 25px rgba(155, 99, 248, 0.15);
+    border-radius: $border-radius;
+
+    &__img {
+      max-width: 40px;
+      max-height: 40px;
+    }
+    &__text {
+      font-family: Montserrat;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 18px;
+      line-height: 22px;
+      color: $color-4;
+    }
+  }
+
+  // Form
+  .form {
+    &__group {
+      margin-bottom: 40px;
+    }
+  }
+
+  // Input-group
+  .input-group {
+    &__text {
+      margin-bottom: 10px;
+      font-family: Montserrat;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 16px;
+      line-height: 20px;
+      color: $color-4;
+    }
+    &__input {
+
+    }
+  }
+</style>
---
+++ src/views/StepOne.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="form section">
+    <div class="container">
+      <div class="section-inner min-vh-100 d-flex flex-column">
+        <div class="flex-grow-1">
+          <div
+            v-if="isSubmissionSuccessful"
+          >
+            <div class="alert section__alert">
+              <div class="d-flex align-center">
+                <div class="flex-child">
+                  <img class="alert__img" src="@/assets/img/success.svg" alt="success">
+                </div>
+              
+                <div class="flex-child">
+                  <p class="alert__text">Ваша заявка принята и находится на стадии обработки.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="form__group">
+            <h2 class="h2 section__h2">Информация об организаторе</h2>
+
+            <label class="input-group">
+              <p class="input-group__text">Организатор</p>
+
+              <input type="text" class="input-group__input input-text w-100">
+            </label>
+          </div>
+
+          <div class="form__group">
+            <h2 class="h2 section__h2">Контактные данные</h2>
+
+            <div class="d-flex">
+              <div class="flex-child flex-child-4">
+                <label class="input-group">
+                  <p class="input-group__text">Телефон</p>
+
+                  <input type="text" class="input-group__input input-text w-100">
+                </label>
+              </div>
+
+              <div class="flex-child flex-child-4">
+                <label class="input-group">
+                  <p class="input-group__text">E-mail</p>
+
+                  <input type="text" class="input-group__input input-text w-100">
+                </label>
+              </div>
+
+              <div class="flex-child flex-child-4">
+                <label class="input-group">
+                  <p class="input-group__text">Город организатора</p>
+
+                  <input type="text" class="input-group__input input-text w-100">
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="form__group">
+            <h2 class="h2 section__h2">Общая информация</h2>
+
+            <label class="input-group">
+              <p class="input-group__text">Название</p>
+
+              <input type="text" class="input-group__input input-text w-100">
+            </label>
+          </div>
+
+          <div class="form__group">
+            <h2 class="h2 section__h2">Фотография</h2>
+
+            <input type="file"
+              v-on:change="setImage"
+            >
+          </div>
+        </div>
+
+        <div class="d-flex">
+          <div class="flex-child">
+            <button class="btn" type="reset">
+              <span>Отменить</span>
+            </button>
+          </div>
+
+          <div class="flex-child">
+            <button class="btn btn_filled" type="button"
+              v-on:click="$emit('next')"
+            >
+              <span>Далее</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [ 'isSubmissionSuccessful' ],
+
+    methods: {
+      setImage(event) {
+        this.$emit('setImage', event)
+      }
+    }
+  }
+</script>
